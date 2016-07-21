9aa12c8f5d4683758d3ccf8597a161ac880cba23
sakoh <sakohartounian@yahoo.com>
2016-07-20 21:14:22 -0700

    tests directories renamed

--- client/wdio.conf.js
+++ client/wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/e2e-tests/**/*.js'
+        './test/e2e/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
